304e39c9ffdd950a7d8f5d1ebd93f188975d4c84
Tao Boli <boli.tao@outlook.com>
2023-06-06 17:22:27 +0800

    lesson2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -5,13 +5,12 @@ import { CommonModule } from '@angular/common';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      home works!
-    </p>
-  `,
-  styleUrls: ['./home.component.css']
+  template: `<section>
+    <form>
+      <input type="text" placeholder="Filter by city" />
+      <button class="primary" type="button">Search</button>
+    </form>
+  </section> `,
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
